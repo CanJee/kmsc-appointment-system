846d5e110c0d2512e97690ecbea51890ef92d34c
Hasan Kanjee <CanJee@users.noreply.github.com>
2017-11-01 21:19:54 -0400

    implement tests for various collection operations (#19)

---
+++ app-tests/members.tests.js
@@ -0,0 +1,20 @@
+if (Meteor.isClient) {
+describe('Members', function () {
+  it('create a member', function () {
+    // This code will be executed by the test driver when the app is started
+    // in the correct mode
+  })
+  it('modify members properties', function () {
+    // This code will be executed by the test driver when the app is started
+    // in the correct mode
+  })
+  it('remove a member by memberID', function () {
+    // This code will be executed by the test driver when the app is started
+    // in the correct mode
+  })
+  it('find a specific member by memberID', function () {
+    // This code will be executed by the test driver when the app is started
+    // in the correct mode
+  })
+})
+}
