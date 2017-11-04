9cb3456713f69cf9ebb3f3585bf1f26438e20593
Hasan Kanjee <CanJee@users.noreply.github.com>
2017-11-04 19:23:58 -0400

    KMSC-43 Report Server connectivity (#22)
    * add fa icon to report server connectivity
    
    * add meteor import
    
    * remove spacejam

--- client/views/common/footer.js
+++ client/views/common/footer.js
@@ -1,7 +1,41 @@
+import { Meteor } from 'meteor/meteor';
+
 Template.footer.rendered = function () {
 
     // FIXED FOOTER
     // Uncomment this if you want to have fixed footer or add 'fixed' class to footer element in html code
     // $('.footer').addClass('fixed');
 
+    Meteor.autorun(function () {
+        var stat;
+        if (Meteor.status().status === "connected") {
+            stat = 'green'
+        }
+        else if (Meteor.status().status === "connecting") {
+            stat = 'yellow'
+        }
+        else {
+          stat = 'red'
+          swal({
+            html:true,
+            title: "Error",
+            text: `Server connection has been lost!`,
+            type: "error",
+          });
+        }
+        Session.set('status',stat);
+    });
+
 };
+
+Template.footer.helpers({
+  green() {
+    return Session.get("status") === 'green';
+  },
+  yellow() {
+    return Session.get("status") === 'yellow';
+  },
+  red() {
+    return Session.get("status") === 'red';
+  }
+});
